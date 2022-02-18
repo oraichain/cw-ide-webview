80aa92b585215953505e3ac8be640cb0d4aeba0c
namwin98 <namwin98@gmail.com>
2022-02-18 15:55:15 +0700

    update style

--- src/components/CustomNetwork/CustomNetwork.jsx
+++ src/components/CustomNetwork/CustomNetwork.jsx
@@ -1,129 +1,154 @@
 import "antd/dist/antd.css";
 import _ from "lodash";
-import { Button, Input, Select, Spin } from 'antd';
-import { ReactComponent as IconSelect } from '../../assets/icons/code.svg';
-import { ReactComponent as IconChain } from '../../assets/icons/chain.svg';
+import { Button, Input, Select, Spin } from "antd";
+import { ReactComponent as IconSelect } from "../../assets/icons/code.svg";
+import { ReactComponent as IconChain } from "../../assets/icons/chain.svg";
 import { useEffect, useState } from "react";
 import { MyDropZone } from "..";
 
 const { Option } = Select;
 
 const CustomNetwork = ({ updateChain }) => {
+  const DEFAULT_CHAINMAME = window.chainStore.current.chainName;
+  const [chainInfos, setChainInfos] = useState(window.chainStore.chainInfos);
+  const [jsonFile, setJsonFile] = useState({});
+  const [jsonFileName, setJsonFileName] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [updateMessage, setUpdateMessage] = useState("");
 
-    const DEFAULT_CHAINMAME = window.chainStore.current.chainName;
-    const [chainInfos, setChainInfos] = useState(window.chainStore.chainInfos);
-    const [jsonFile, setJsonFile] = useState({});
-    const [jsonFileName, setJsonFileName] = useState("");
-    const [errorMessage, setErrorMessage] = useState("");
-    const [updateMessage, setUpdateMessage] = useState("");
+  const handleJsonFile = (file) => {
+    setJsonFile(file.content);
+    setJsonFileName(file.fileName);
+    setUpdateMessage("");
+  };
 
-    const handleJsonFile = (file) => {
-        setJsonFile(file.content);
-        setJsonFileName(file.fileName);
-        setUpdateMessage("");
+  const onAddChain = () => {
+    try {
+      setErrorMessage("");
+      console.log("json file: ", jsonFile);
+      if (jsonFile.chainId) {
+        window.chainStore.addChain(jsonFile);
+        // set chain to auto trigger new chain store
+        setChainInfos(window.chainStore.chainInfos);
+        setUpdateMessage("Successfully added the new chain");
+      } else throw "Invalid chain data";
+    } catch (error) {
+      setErrorMessage(String(error));
+      setUpdateMessage("");
     }
+  };
 
-    const onAddChain = () => {
-        try {
-            setErrorMessage("");
-            console.log("json file: ", jsonFile)
-            if (jsonFile.chainId) {
-                window.chainStore.addChain(jsonFile);
-                // set chain to auto trigger new chain store
-                setChainInfos(window.chainStore.chainInfos);
-                setUpdateMessage("Successfully added the new chain")
-            } else throw "Invalid chain data"
-        } catch (error) {
-            setErrorMessage(String(error));
-            setUpdateMessage("");
-        }
+  const onRemoveChain = () => {
+    try {
+      setErrorMessage("");
+      if (jsonFile.chainId) {
+        window.chainStore.removeChain(jsonFile.chainId);
+        // set chain to auto trigger new chain store
+        setChainInfos(window.chainStore.chainInfos);
+        setUpdateMessage("Successfully removed the provided chain");
+      } else throw "invalid chain data";
+    } catch (error) {
+      setErrorMessage(String(error));
+      setUpdateMessage("");
     }
+  };
 
-    const onRemoveChain = () => {
-        try {
-            setErrorMessage("");
-            if (jsonFile.chainId) {
-                window.chainStore.removeChain(jsonFile.chainId);
-                // set chain to auto trigger new chain store
-                setChainInfos(window.chainStore.chainInfos);
-                setUpdateMessage("Successfully removed the provided chain")
-            } else throw "invalid chain data"
-        } catch (error) {
-            setErrorMessage(String(error));
-            setUpdateMessage("");
-        }
-    }
-
-
-    return (
-        <div className="chain-select">
-            <div style={{ display: "flex", alignItems: "center" }}>
-                <IconChain
-                    style={{
-                        width: "16px",
-                        height: "16px",
-                        marginRight: "5px",
-                        marginBottom: "8px",
-                    }}
-                />
-                <h3> Select chain name</h3>
-            </div>
-            <Select
-                defaultValue={DEFAULT_CHAINMAME}
-                style={{ width: 240 }}
-                suffixIcon={<IconSelect />}
-                onSelect={(value) => {
-                    window.chainStore.setChain(value);
-                    updateChain(value);
-                    // setChainName(value);
-                    // setGasPrice(window.chainStore.current.gasPriceStep?.average ? window.chainStore.current.gasPriceStep.average.toString() : "0");
-                    // setGasDenom(window.chainStore.current.feeCurrencies[0].coinMinimalDenom);
-                }}
-            >
-                {chainInfos.map((info) => (
-                    <Option key={info.chainName} value={info.chainName}>
-                        {info.chainName}
-                    </Option>
-                ))}
-            </Select>
-            <div className="chain-management">
-                <div className="update-chain">
-                    <Button onClick={onAddChain}>
-                        Add new chain
-                    </Button>
-                    <Button onClick={onRemoveChain}>
-                        Remove chain
-                    </Button>
-                </div>
-                {(_.isEmpty(jsonFile) &&
-                    <div style={{ display: 'flex' }}>
-                        <MyDropZone setSchema={null} setJson={handleJsonFile} dropZoneText={"Drop the chain info json file here to add / remove chain"} />
-                    </div>
-                )}
-                {(jsonFileName && (
-                    <div>
-                        <div style={{ display: 'flex', color: 'white' }}>
-                            {`file name: ${jsonFileName}`}
-                        </div>
-                        <Button onClick={() => { setJsonFile({}); setJsonFileName(""); setUpdateMessage(""); setErrorMessage("") }}>
-                            Remove json chain
-                        </Button>
-                    </div>
-                ))}
-                {(errorMessage && (
-                    <div className="contract-address">
-                        <span style={{ color: "red" }}>Error message </span>
-                        <p>{errorMessage}</p>
-                    </div>
-                ))}
-                {(updateMessage && (
-                    <div className="contract-address">
-                        <p>{updateMessage}</p>
-                    </div>
-                ))}
-            </div>
+  return (
+    <div className="chain-select">
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <IconChain
+          style={{
+            width: "16px",
+            height: "16px",
+            marginRight: "5px",
+            marginBottom: "8px",
+          }}
+        />
+        <h3> Select chain name</h3>
+      </div>
+      <Select
+        defaultValue={DEFAULT_CHAINMAME}
+        style={{ width: 240 }}
+        suffixIcon={<IconSelect />}
+        onSelect={(value) => {
+          window.chainStore.setChain(value);
+          updateChain(value);
+          // setChainName(value);
+          // setGasPrice(window.chainStore.current.gasPriceStep?.average ? window.chainStore.current.gasPriceStep.average.toString() : "0");
+          // setGasDenom(window.chainStore.current.feeCurrencies[0].coinMinimalDenom);
+        }}
+      >
+        {chainInfos.map((info) => (
+          <Option key={info.chainName} value={info.chainName}>
+            {info.chainName}
+          </Option>
+        ))}
+      </Select>
+      <div className="chain-management">
+        <div className="update-chain">
+          <Button onClick={onAddChain} className="primary-button">
+            Add new chain
+          </Button>
+          <Button onClick={onRemoveChain} className="remove-button">
+            Remove chain
+          </Button>
         </div>
-    )
-}
+        {_.isEmpty(jsonFile) && (
+          <div
+            style={{
+              display: "flex",
+              cursor: "pointer",
+              fontFamily: "Courier",
+            }}
+          >
+            <MyDropZone
+              setSchema={null}
+              setJson={handleJsonFile}
+              dropZoneText={
+                "Drop the chain info json file here to add / remove chain"
+              }
+            />
+          </div>
+        )}
+        {jsonFileName && (
+          <div>
+            <div
+              style={{
+                display: "flex",
+                color: "#c4c6c9",
+                fontFamily: "Courier",
+                paddingBottom: "8px",
+              }}
+            >
+              {`file name: ${jsonFileName}`}
+            </div>
+            <Button
+              onClick={() => {
+                setJsonFile({});
+                setJsonFileName("");
+                setUpdateMessage("");
+                setErrorMessage("");
+              }}
+              className="remove-secondary"
+            >
+              Remove json chain
+            </Button>
+          </div>
+        )}
+        {errorMessage && (
+          <div className="contract-address">
+            <span style={{ color: "red" }}>Error message </span>
+            <p>{errorMessage}</p>
+          </div>
+        )}
+        {updateMessage && (
+          <div className="contract-address">
+            <p>{updateMessage}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default CustomNetwork;
--- src/pages/AdvancedInteraction/AdvancedInteraction.tsx
+++ src/pages/AdvancedInteraction/AdvancedInteraction.tsx
@@ -1,167 +1,267 @@
 import { useRef, useState } from "react";
 // import Form from "@rjsf/core";
-import '../../themes/style.scss';
-import { Button, Spin } from 'antd';
-import { LoadingOutlined } from '@ant-design/icons';
+import "../../themes/style.scss";
+import { Button, Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
 import _ from "lodash";
-import ReactJson from 'react-json-view';
+import ReactJson from "react-json-view";
 import CosmJsFactory from "src/lib/cosmjs-factory";
-import { CustomForm, CustomInput, GasForm, MyDropZone, HandleOptions, CustomSelect, CustomNetwork } from "src/components";
+import {
+  CustomForm,
+  CustomInput,
+  GasForm,
+  MyDropZone,
+  HandleOptions,
+  CustomSelect,
+  CustomNetwork,
+} from "src/components";
 
 const antIcon = (
-    <LoadingOutlined style={{ fontSize: 24, color: "#7954FF" }} spin />
+  <LoadingOutlined style={{ fontSize: 24, color: "#7954FF" }} spin />
 );
 
 const AdvancedInteraction = () => {
-    const DEFAULT_CHAINMAME = window.chainStore.current.chainName;
-    const [mnemonic, setMnemonic] = useState('');
-    const [gasPrice, setGasPrice] = useState(window.chainStore.current.gasPriceStep?.average ? window.chainStore.current.gasPriceStep.average.toString() : "0");
-    const [gasDenom, setGasDenom] = useState(window.chainStore.current.feeCurrencies[0].coinMinimalDenom);
-    const [gasLimit, setGasLimit] = useState(undefined);
-    const [chainName, setChainName] = useState(DEFAULT_CHAINMAME);
-    const [interactOption, setInteractOption] = useState("query");
-    const [contractAddr, setContractAddr] = useState("");
-    const [errorMessage, setErrorMessage] = useState("");
-    const [resultJson, setResultJson] = useState({});
-    const [isInteractionLoading, setIsInteractionLoading] = useState(false);
-    const [queryMessage, setQueryMessage] = useState("");
-    const [executeMessage, setExecuteMessage] = useState("");
-    const [querySchema, setQuerySchema] = useState({});
-    const [handleSchema, setHandleSchema] = useState({});
-    const handleOptionsRef = useRef(null);
+  const DEFAULT_CHAINMAME = window.chainStore.current.chainName;
+  const [mnemonic, setMnemonic] = useState("");
+  const [gasPrice, setGasPrice] = useState(
+    window.chainStore.current.gasPriceStep?.average
+      ? window.chainStore.current.gasPriceStep.average.toString()
+      : "0"
+  );
+  const [gasDenom, setGasDenom] = useState(
+    window.chainStore.current.feeCurrencies[0].coinMinimalDenom
+  );
+  const [gasLimit, setGasLimit] = useState(undefined);
+  const [chainName, setChainName] = useState(DEFAULT_CHAINMAME);
+  const [interactOption, setInteractOption] = useState("query");
+  const [contractAddr, setContractAddr] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [resultJson, setResultJson] = useState({});
+  const [isInteractionLoading, setIsInteractionLoading] = useState(false);
+  const [queryMessage, setQueryMessage] = useState("");
+  const [executeMessage, setExecuteMessage] = useState("");
+  const [querySchema, setQuerySchema] = useState({});
+  const [handleSchema, setHandleSchema] = useState({});
+  const handleOptionsRef = useRef(null);
 
-    const updateChain = (value) => {
-        setChainName(value);
-        setGasPrice(window.chainStore.current.gasPriceStep?.average ? window.chainStore.current.gasPriceStep.average.toString() : "0");
-        setGasDenom(window.chainStore.current.feeCurrencies[0].coinMinimalDenom);
-    }
-
-    const onQuery = async (data) => {
-        setErrorMessage("");
-        setIsInteractionLoading(true);
-        let cosmJs = new CosmJsFactory(window.chainStore.current);
-        try {
-            let finalMessage = queryMessage;
-            if (data) finalMessage = JSON.stringify(data);
-            const queryResult = await cosmJs.current.query(contractAddr, finalMessage);
-            console.log("query result: ", queryResult);
-            setResultJson({ data: queryResult });
-        } catch (error) {
-            setErrorMessage(String(error));
-        }
-        setIsInteractionLoading(false);
-    }
-
-    const onHandle = async (data) => {
-        setErrorMessage("");
-        setIsInteractionLoading(true);
-        let cosmJs = new CosmJsFactory(window.chainStore.current);
-        try {
-            let finalMessage = executeMessage;
-            if (data) finalMessage = JSON.stringify(data);
-            const queryResult = await cosmJs.current.execute({
-                mnemonic, address: contractAddr, handleMsg: finalMessage, gasAmount: { amount: gasPrice, denom: gasDenom }, gasLimits: {
-                    exec: gasLimit ? parseInt(gasLimit) : undefined
-                }, handleOptions: handleOptionsRef.current,
-            });
-            console.log("query result: ", queryResult);
-            setResultJson({ data: queryResult });
-        } catch (error) {
-            setErrorMessage(String(error));
-        }
-        setIsInteractionLoading(false);
-    }
-
-    return (
-        <div className="app-body">
-            <div className="intro">
-                Start the Wasm smart contract development journey with CosmWasm IDE by building your first contract! Choose a smart contract file and click the button 'Build CosmWasm' to build your contract. You can also interact with an existing smart contract.
-            </div>
-            <CustomNetwork updateChain={updateChain} />
-            <div className="wrap-form">
-                <CustomInput inputHeader="Contract address" input={contractAddr} setInput={setContractAddr} placeholder="eg. orai1ars73g86y4kzajsgam5ee38npgmkq54dlzuz6w" />
-                <CustomInput inputHeader="Wallet mnemonic (optional)" input={mnemonic} setInput={setMnemonic} placeholder="eg. foo bar" />
-
-            </div>
-
-            <CustomSelect setInteractOption={setInteractOption} />
-
-            {interactOption === "execute" &&
-                <div>
-                    <div className="contract-address">
-                        <span>Contract Execute </span>
-                    </div>
-                    <div className="wrap-form">
-                        <GasForm gasPrice={gasPrice} setGasPrice={setGasPrice} gasDenom={gasDenom} setGasDenom={setGasDenom} gasLimit={gasLimit} setGasLimit={setGasLimit} />
-                        <HandleOptions handleOptionsRef={handleOptionsRef} />
-                        {_.isEmpty(handleSchema) &&
-                            <div style={{ marginBottom: '10px' }}>
-                                <CustomInput inputHeader="Execute message" input={executeMessage} setInput={setExecuteMessage} placeholder="eg. {}" />
-                                <Button onClick={() => { onHandle(null) }}>
-                                    Execute
-                                </Button>
-                                <MyDropZone setSchema={setHandleSchema} setJson={null} dropZoneText={"Upload the schema file"} />
-                            </div>
-                        }
-                        {!_.isEmpty(handleSchema) && <div>
-                            <CustomForm schema={handleSchema} onSubmit={(data) => onHandle(data)} />
-                            <Button onClick={() => { setHandleSchema({}) }}>
-                                Remove schema form
-                            </Button>
-                        </div>
-                        }
-                    </div>
-                </div>
-            }
-            {interactOption === "query" &&
-                <div>
-                    <div className="contract-address">
-                        <span>Contract Query </span>
-                    </div>
-                    <div className="wrap-form">
-                        <CustomInput inputHeader="Gas denom" input={gasDenom} setInput={setGasDenom} placeholder="eg. orai" />
-                        {_.isEmpty(querySchema) && <div>
-                            <CustomInput inputHeader="Query message" input={queryMessage} setInput={setQueryMessage} placeholder="eg. {}" />
-                            <Button onClick={() => onQuery(null)}>
-                                Query
-                            </Button>
-                            <MyDropZone setSchema={setQuerySchema} setJson={null} dropZoneText={"Upload the schema file"} />
-                        </div>
-                        }
-                        {!_.isEmpty(querySchema) &&
-                            <div style={{ marginBottom: '10px' }}>
-                                <CustomForm schema={querySchema} onSubmit={(data) => onQuery(data)} />
-                                <Button onClick={() => { setQuerySchema({}) }}>
-                                    Remove schema form
-                                </Button>
-                            </div>
-                        }
-                    </div>
-                </div>
-            }
-            <div className="app-divider" />
-
-            {!isInteractionLoading ?
-                (errorMessage && (
-                    <div className="contract-address">
-                        <span style={{ color: "red" }}>Error message </span>
-                        <p>{errorMessage}</p>
-                    </div>
-                )
-                ) : (
-                    <div className="deploying">
-                        <Spin indicator={antIcon} />
-                        <span>Invoking ...</span>
-                    </div>
-                )
-            }
-            {!_.isEmpty(resultJson) && (
-                <div style={{ marginTop: '10px' }}>
-                    <ReactJson collapseStringsAfterLength={20} name={false} displayObjectSize={false} src={resultJson} theme={"ocean"} />
-                </div>
-            )}
-        </div>
+  const updateChain = (value) => {
+    setChainName(value);
+    setGasPrice(
+      window.chainStore.current.gasPriceStep?.average
+        ? window.chainStore.current.gasPriceStep.average.toString()
+        : "0"
     );
+    setGasDenom(window.chainStore.current.feeCurrencies[0].coinMinimalDenom);
+  };
+
+  const onQuery = async (data) => {
+    setErrorMessage("");
+    setIsInteractionLoading(true);
+    let cosmJs = new CosmJsFactory(window.chainStore.current);
+    try {
+      let finalMessage = queryMessage;
+      if (data) finalMessage = JSON.stringify(data);
+      const queryResult = await cosmJs.current.query(
+        contractAddr,
+        finalMessage
+      );
+      console.log("query result: ", queryResult);
+      setResultJson({ data: queryResult });
+    } catch (error) {
+      setErrorMessage(String(error));
+    }
+    setIsInteractionLoading(false);
+  };
+
+  const onHandle = async (data) => {
+    setErrorMessage("");
+    setIsInteractionLoading(true);
+    let cosmJs = new CosmJsFactory(window.chainStore.current);
+    try {
+      let finalMessage = executeMessage;
+      if (data) finalMessage = JSON.stringify(data);
+      const queryResult = await cosmJs.current.execute({
+        mnemonic,
+        address: contractAddr,
+        handleMsg: finalMessage,
+        gasAmount: { amount: gasPrice, denom: gasDenom },
+        gasLimits: {
+          exec: gasLimit ? parseInt(gasLimit) : undefined,
+        },
+        handleOptions: handleOptionsRef.current,
+      });
+      console.log("query result: ", queryResult);
+      setResultJson({ data: queryResult });
+    } catch (error) {
+      setErrorMessage(String(error));
+    }
+    setIsInteractionLoading(false);
+  };
+
+  return (
+    <div className="app-body">
+      <div className="intro">
+        Start the Wasm smart contract development journey with CosmWasm IDE by
+        building your first contract! Choose a smart contract file and click the
+        button 'Build CosmWasm' to build your contract. You can also interact
+        with an existing smart contract.
+      </div>
+      <CustomNetwork updateChain={updateChain} />
+      <div className="wrap-form">
+        <CustomInput
+          inputHeader="Contract address"
+          input={contractAddr}
+          setInput={setContractAddr}
+          placeholder="eg. orai1ars73g86y4kzajsgam5ee38npgmkq54dlzuz6w"
+        />
+        <CustomInput
+          inputHeader="Wallet mnemonic (optional)"
+          input={mnemonic}
+          setInput={setMnemonic}
+          placeholder="eg. foo bar"
+        />
+      </div>
+
+      <CustomSelect setInteractOption={setInteractOption} />
+
+      {interactOption === "execute" && (
+        <div>
+          <div className="contract-address">
+            <span>Contract Execute </span>
+          </div>
+          <div className="wrap-form">
+            <GasForm
+              gasPrice={gasPrice}
+              setGasPrice={setGasPrice}
+              gasDenom={gasDenom}
+              setGasDenom={setGasDenom}
+              gasLimit={gasLimit}
+              setGasLimit={setGasLimit}
+            />
+            <HandleOptions handleOptionsRef={handleOptionsRef} />
+            {_.isEmpty(handleSchema) && (
+              <div style={{ marginBottom: "10px" }}>
+                <CustomInput
+                  inputHeader="Execute message"
+                  input={executeMessage}
+                  setInput={setExecuteMessage}
+                  placeholder="eg. {}"
+                />
+                <Button
+                  onClick={() => {
+                    onHandle(null);
+                  }}
+                >
+                  Execute
+                </Button>
+                <div style={{ cursor: "pointer", fontFamily: "Courier" }}>
+                  <MyDropZone
+                    setSchema={setHandleSchema}
+                    setJson={null}
+                    dropZoneText={"Upload the schema file"}
+                  />
+                </div>
+              </div>
+            )}
+            {!_.isEmpty(handleSchema) && (
+              <div>
+                <CustomForm
+                  schema={handleSchema}
+                  onSubmit={(data) => onHandle(data)}
+                />
+                <Button
+                  onClick={() => {
+                    setHandleSchema({});
+                  }}
+                >
+                  Remove schema form
+                </Button>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+      {interactOption === "query" && (
+        <div>
+          <div className="contract-address">
+            <span>Contract Query </span>
+          </div>
+          <div className="wrap-form">
+            <CustomInput
+              inputHeader="Gas denom"
+              input={gasDenom}
+              setInput={setGasDenom}
+              placeholder="eg. orai"
+            />
+            {_.isEmpty(querySchema) && (
+              <div style={{ marginBottom: "24px" }}>
+                <CustomInput
+                  inputHeader="Query message"
+                  input={queryMessage}
+                  setInput={setQueryMessage}
+                  placeholder="eg. {}"
+                />
+                <Button
+                  onClick={() => onQuery(null)}
+                  className="primary-button"
+                >
+                  Query
+                </Button>
+                <div style={{ cursor: "pointer", fontFamily: "Courier" }}>
+                  <MyDropZone
+                    setSchema={setQuerySchema}
+                    setJson={null}
+                    dropZoneText={"Upload the schema file"}
+                  />
+                </div>
+              </div>
+            )}
+            {!_.isEmpty(querySchema) && (
+              <div style={{ marginBottom: "10px" }}>
+                <CustomForm
+                  schema={querySchema}
+                  onSubmit={(data) => onQuery(data)}
+                />
+                <Button
+                  onClick={() => {
+                    setQuerySchema({});
+                  }}
+                >
+                  Remove schema form
+                </Button>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+      <div className="app-divider" />
+
+      {!isInteractionLoading ? (
+        errorMessage && (
+          <div className="contract-address">
+            <span style={{ color: "red" }}>Error message </span>
+            <p>{errorMessage}</p>
+          </div>
+        )
+      ) : (
+        <div className="deploying">
+          <Spin indicator={antIcon} />
+          <span>Invoking ...</span>
+        </div>
+      )}
+      {!_.isEmpty(resultJson) && (
+        <div style={{ marginTop: "10px" }}>
+          <ReactJson
+            collapseStringsAfterLength={20}
+            name={false}
+            displayObjectSize={false}
+            src={resultJson}
+            theme={"ocean"}
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default AdvancedInteraction;
