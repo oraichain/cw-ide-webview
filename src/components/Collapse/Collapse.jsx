f00964db42e1d836133fa01578ca0220a0cc0893
Duc Pham Le <44611780+ducphamle2@users.noreply.github.com>
2022-04-04 11:32:02 +0700

    Feat/change owner (#8)
    Maintain contract states for multiple contract interaction
    
    Co-authored-by: czaylove97 <nguyenvanhauit1997@gmail.com>
    Co-authored-by: haunv <haunv@haunvs-MacBook-Pro.local>

---
+++ src/components/Collapse/Collapse.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Collapse } from "react-collapse";
+import RemoveIcon from "../../remove.png";
+import { Popconfirm } from "antd";
+
+class DropdownItem extends React.Component {
+  state = {
+    isDropdownOpen: false,
+  };
+
+  onDropdownClicked = () => {
+    this.setState((prevState) => ({
+      isDropdownOpen: !prevState.isDropdownOpen,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              maxWidth: 500,
+            }}
+          >
+            <p onClick={this.onDropdownClicked} className="click" >{this.props.title}</p>
+            <Popconfirm
+              onConfirm={this.props.removeContract}
+              title="delete this address?"
+              okText="Yes"
+              placement="top"
+              cancelText="No"
+            >
+              <img className="click" src={RemoveIcon} width={20} height={20} alt="" srcset="" />
+            </Popconfirm>
+          </div>
+        </div>
+        <Collapse isOpened={this.state.isDropdownOpen}>
+          <div>{this.props.children}</div>
+        </Collapse>
+      </div>
+    );
+  }
+}
+
+export default DropdownItem;
