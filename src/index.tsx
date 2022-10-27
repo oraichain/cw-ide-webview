f4ad87a878a96d62d63ef4c0d78b402e10323243
im6h <danghaiv.18@gmail.com>
2022-10-27 18:11:23 +0700

    add catch os platform.

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import * as ReactDOM from 'react-dom';
-import './polyfill';
-import App from './App';
-import './index.css';
-import Keplr from 'src/lib/Keplr';
-import { ChainStore } from './stores/chain';
-import { EmbedChainInfos } from './config';
+import * as ReactDOM from "react-dom";
+import "./polyfill";
+import App from "./App";
+import "./index.css";
+import Keplr from "src/lib/Keplr";
+import { ChainStore } from "./stores/chain";
+import { EmbedChainInfos } from "./config";
+import * as os from "os";
 
 window.chainStore = new ChainStore(EmbedChainInfos);
 
@@ -12,29 +13,32 @@ window.chainStore = new ChainStore(EmbedChainInfos);
 // global Keplr
 window.Keplr = new Keplr();
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
 
-let ctrlDown = false;
-const ctrlKey = 'Meta';
+if (navigator.platform.indexOf('Mac') > -1) {
+  let ctrlDown = false;
+  const ctrlKey = "Meta";
 
-document.addEventListener('keydown', (e) => {
-  if (e.key === ctrlKey) ctrlDown = true;
-});
-document.addEventListener('keyup', (e) => {
-  if (e.key === ctrlKey) ctrlDown = false;
-});
+  const commandMap = {
+    a: "selectAll",
+    c: "copy",
+    v: "paste",
+    x: "cut",
+    z: "undo",
+    shift_z: "redo"
+  };
 
-document.addEventListener('keydown', (e) => {
-  if (ctrlDown) {
-    switch (e.key) {
-      case 'v':
-        return document.execCommand('paste');
-      case 'c':
-        return document.execCommand('copy');
-      case 'x':
-        return document.execCommand('cut');
-      case 'a':
-        return document.execCommand('selectAll');
+  document.addEventListener("keydown", e => {
+    if (e.key === ctrlKey) ctrlDown = true;
+  });
+  document.addEventListener("keyup", e => {
+    if (e.key === ctrlKey) ctrlDown = false;
+  });
+
+  document.addEventListener("keydown", e => {
+    if (ctrlDown) {
+      let command = commandMap[e.shiftKey ? "shift_" : "" + e.key];
+      if (command) document.execCommand(command);
     }
-  }
-});
+  });
+}
